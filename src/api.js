c7d96095170a2a6649c572472b7b0caebc4a0180
AlvaroNarizonRui <aruiz@iesrafaelalberti.es>
2025-03-14 22:59:56 +0100

    cambiada url para conectar con el back

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "https://fitcenterapifinal-production-4216.up.railway.app/api",
     withCredentials: true, // Puedes dejarlo si usas cookies, pero para tokens no es necesario
 });
 
